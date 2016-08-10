c34657e0fcb563c5d66beeaaa2eebbc6f97c0928
mary1611 <mary1611@yandex.ru>
2016-08-10 13:44:39 +0300

    шарики

--- catch the ball/game_canvas.py
+++ catch the ball/game_canvas.py
@@ -9,15 +9,18 @@ def paint(event):
     if event.widget != canvas:
         print('Странно, ведь paint()привязывали только к Canvas..')
         return
-    canvas.coords(line, 0, 0, event_x, event_y)
+    canvas.coords(line, 0, 0, event.x, event.y)
+
 
 root = tkinter.Tk()
 
-canvas = tkinter.Canvas(root)
-canvas.bird("<Motion>", paint)
+canvas = tkinter.Canvas(root, bg='white', width=400, height=400)
+canvas.bind("<Motion>", paint)
 canvas.pack()
 
 line = canvas.create_line(0,0,10, 10)
-
+for i in range(10):
+    canvas.create_oval(i*40, i*40, i*40+10, i*40+10, fill='green', width=2)
+    
 root.mainloop()
 print ("это будет только при выходе из виджета")
